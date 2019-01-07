266c0f9be676a118ca8660e526aff0e561c84f0a
Huỳnh Hà <huynhsamha@gmail.com>
2019-01-07 09:54:54 +0700

    add problem

--- js/two-sum.js
+++ js/two-sum.js
@@ -3,14 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  hash = {}
-  for (let i in nums) {
-    v = nums[i]
-    if (hash[target-v] != null) {
-      return [hash[target-v], i]
+var twoSum = function (nums, target) {
+    hash = {}
+    for (let i in nums) {
+        v = nums[i]
+        if (hash[target - v] != null) {
+            return [hash[target - v], i]
+        }
+        hash[v] = i
     }
-    hash[v] = i
-  }
-  return null
+    return null
 };
